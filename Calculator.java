fa8ead0227539c6df6ef0b3a3930e3ae956b1e84
rpekash <ksyla@quinnipiac.edu>
2024-02-07 19:28:28 -0500

    input floating point numbers.

--- Calculator.java
+++ Calculator.java
@@ -3,26 +3,26 @@ import java.util.Scanner;
 public class Calculator{
     public static void main(String[] args){
         Scanner userInput = new Scanner(System.in);
-        System.out.println("Please enter in two whole numbers.");
-        int numberOne = userInput.nextInt() ;
-        int numberTwo = userInput.nextInt() ;
+        System.out.println("Please enter in two numbers.");
+        float numberOne= userInput.nextFloat();
+        float numberTwo= userInput.nextFloat();
         System.out.println("Choose One Operation by entering corresponding number, 1. Addition 2. Subtraction 3. Multiply 4. Divide");
         int operationChoice = userInput.nextInt() ;
 
         if(operationChoice == 1){
-            int sum = numberOne + numberTwo;
+            float sum = numberOne + numberTwo;
             System.out.println(sum);
         }
         else if (operationChoice == 2){
-            int sub = numberOne - numberTwo;
+            float sub = numberOne - numberTwo;
             System.out.println(sub);
         }
         else if (operationChoice == 3){
-            int multi = numberOne*numberTwo;
+            float multi = numberOne*numberTwo;
             System.out.println(multi);
         }
         else if (operationChoice == 4){
-            int divide = numberOne/numberTwo;
+            float divide = numberOne/numberTwo;
             System.out.println(divide);
         }
         
