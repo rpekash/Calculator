a247fd1a5e680d61e177f93ed6c336c35ace4539
rpekash <ksyla@quinnipiac.edu>
2024-02-07 19:10:40 -0500

    Subtraction added with options.

--- Calculator.java
+++ Calculator.java
@@ -2,13 +2,22 @@ import java.util.Scanner;
 
 public class Calculator{
     public static void main(String[] args){
-        System.out.println("Please enter in two whole numbers.");
         Scanner userInput = new Scanner(System.in);
+        System.out.println("Please enter in two whole numbers.");
         int numberOne = userInput.nextInt() ;
         int numberTwo = userInput.nextInt() ;
-        int sum = numberOne + numberTwo;
-        System.out.println(sum);
+        System.out.println("Choose One Operation by entering corresponding number, 1. Addition 2. Subtraction");
+        int operationChoice = userInput.nextInt() ;
 
+        if(operationChoice == 1){
+            int sum = numberOne + numberTwo;
+            System.out.println(sum);
+        }
+        else{
+            int sub = numberOne - numberTwo;
+            System.out.println(sub);
+        }
+        
 
     }
 }
