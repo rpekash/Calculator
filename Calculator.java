b6afcd5b71e0c8c3ce16cf4eca550c4838e0e03c
rpekash <ksyla@quinnipiac.edu>
2024-02-07 19:16:01 -0500

    Multiply and Divide added.

--- Calculator.java
+++ Calculator.java
@@ -6,16 +6,24 @@ public class Calculator{
         System.out.println("Please enter in two whole numbers.");
         int numberOne = userInput.nextInt() ;
         int numberTwo = userInput.nextInt() ;
-        System.out.println("Choose One Operation by entering corresponding number, 1. Addition 2. Subtraction");
+        System.out.println("Choose One Operation by entering corresponding number, 1. Addition 2. Subtraction 3. Multiply 4. Divide");
         int operationChoice = userInput.nextInt() ;
 
         if(operationChoice == 1){
             int sum = numberOne + numberTwo;
             System.out.println(sum);
         }
-        else{
+        else if (operationChoice == 2){
             int sub = numberOne - numberTwo;
             System.out.println(sub);
+        }
+        else if (operationChoice == 3){
+            int multi = numberOne*numberTwo;
+            System.out.println(multi);
+        }
+        else if (operationChoice == 4){
+            int divide = numberOne/numberTwo;
+            System.out.println(divide);
         }
         
 
